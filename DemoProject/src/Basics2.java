fe8c999b5c46cc51488918904f08ce52429a4bed
Akshay Harugop <99660258+AkshayHarugop@users.noreply.github.com>
2025-09-03 14:46:58 +0530

    Adding some additional code
    Adding some additional code

--- DemoProject/src/Basics2.java
+++ DemoProject/src/Basics2.java
@@ -1,6 +1,8 @@
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 public class Basics2 {
 
@@ -12,7 +14,7 @@ public class Basics2 {
         then - Validate the response*/
 		
 		RestAssured.baseURI= "https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body("{\r\n"
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body("{\r\n"
 				+ "  \"location\": {\r\n"
 				+ "    \"lat\": -38.383494,\r\n"
 				+ "    \"lng\": 33.427362\r\n"
@@ -30,13 +32,87 @@ public class Basics2 {
 				+ "}")
 		.when().post("/maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200)
-		.body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)");
-		
-		
+		.body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 		
 //		equalsTo() method belongs to static package so it won't import the code automatically so we need to add the import manually and update the package..
 //		import static org.hamcrest.Matchers.*;
-
+		System.out.println("------------------------------------");
+		System.out.println(response);
+		System.out.println("------------------------------------");
+		
+		JsonPath jp = new JsonPath(response);
+		String PlaceId = jp.getString("place_id");
+		String newAdd = "Bangalore";
+		
+		System.out.println(PlaceId);
+		
+		/*Validate if Get place API is working as expected 
+        will use methods like given, when and there
+        given - all input details (query parameter, header) , Body
+        when -  submit the API CRUD action (resource and http method goes under when)
+        then - Validate the response*/
+		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", PlaceId)
+		.when().get("/maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(200)
+		.body("address", equalTo("29, side layout, cohen 09")).body("accuracy", equalTo("50")).header("Server", "Apache/2.4.52 (Ubuntu)");
+		
+		System.out.println("------------------------------------");
+		
+		/*Validate if Put place (Update) API is working as expected 
+        will use methods like given, when and there
+        given - all input details (query parameter, header) , Body
+        when -  submit the API CRUD action (resource and http method goes under when)
+        then - Validate the response*/
+		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", PlaceId).body("{\r\n"
+				+ "\"place_id\":\""+PlaceId+"\",\r\n"
+				+ "\"address\":\"70 winter walk, USA\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}")
+		.when().put("/maps/api/place/update/json")
+		.then().log().all().assertThat().statusCode(200)
+		.body("msg", equalTo("Address successfully updated"));
+		
+		System.out.println("------------------------------------");
+		
+		/*Validate if Get place API is working as expected 
+        will use methods like given, when and there
+        given - all input details (query parameter, header) , Body
+        when -  submit the API CRUD action (resource and http method goes under when)
+        then - Validate the response*/
+		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", PlaceId)
+		.when().get("/maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(200)
+		.body("address", equalTo("70 winter walk, USA")).body("accuracy", equalTo("50")).header("Server", "Apache/2.4.52 (Ubuntu)");
+		
+		System.out.println("------------------------------------");
+		
+		/*Validate if delete place API is working as expected 
+        will use methods like given, when and there
+        given - all input details (query parameter, header) , Body
+        when -  submit the API CRUD action (resource and http method goes under when)
+        then - Validate the response*/
+		
+		given().log().all().queryParam("key", "qaclick123").body("{\r\n"
+				+ "    \"place_id\":\""+PlaceId+"\"\r\n"
+				+ "}")
+		.when().delete("/maps/api/place/delete/json")
+		.then().log().all().assertThat().statusCode(200)
+		.body("status", equalTo("OK")).header("Server", "Apache/2.4.52 (Ubuntu)");
+		
+		System.out.println("------------------------------------");
+		
+		/*Validate if Get place API is working as expected 
+        will use methods like given, when and there
+        given - all input details (query parameter, header) , Body
+        when -  submit the API CRUD action (resource and http method goes under when)
+        then - Validate the response*/
+		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", PlaceId)
+		.when().get("/maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(404)
+		.body("msg", equalTo("Get operation failed, looks like place_id  doesn't exists")).header("Server", "Apache/2.4.52 (Ubuntu)");
+		
+		System.out.println("------------------------------------");
+		
 	}
 
 }
