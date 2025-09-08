ba24965757c4d91bac631379248f84fd97540c00
Akshay Harugop <99660258+AkshayHarugop@users.noreply.github.com>
2025-09-08 22:16:00 +0530

    Update expected status code for Jira issue creation
    Changed the expected HTTP status code from 200 to 201 in the Jira issue creation API call to correctly reflect the standard response for successful resource creation.

--- DemoProject/src/Jira.java
+++ DemoProject/src/Jira.java
@@ -17,7 +17,7 @@ public class Jira {
 		String createBugResponse = given().log().all().header("Accept","application/json").header("Content-Type","application/json").header("Authorization","Basic YWtzaGF5aGFydWdvcEBnbWFpbC5jb206QVRBVFQzeEZmR0YwS093V3Jlc3g4bmRaSGxGYnNqQkZpczhEUUJDZVlKbk1aR1E0eXJlYXZ2VEZJUHUxRC1jOHpZLUFBek84dm5iNG5XWXhqdTF3dUlmRkxIdU9oU1M4WUJCWTlQWWxTMHk5ODR4NlFzTzhIN0kzZnp5WnNVWlF6VkhDeDlURHoyNUFaQllSOFBVel8yZ2owcFpZS083TWNiRGNCbHg5cktsZzF2enlPakN1dXdzPTJCODUyQUFD")
 		.body(payload.jiraCreateDefect())
 		.when().post("/rest/api/2/issue")
-		.then().log().all().assertThat().statusCode(200).extract().response().asString();
+		.then().log().all().assertThat().statusCode(201).extract().response().asString();
 	}
 	
 	
