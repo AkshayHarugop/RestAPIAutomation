83515bfc72772acea550694d189122e9b4ee8584
Akshay Harugop <99660258+AkshayHarugop@users.noreply.github.com>
2025-10-19 14:11:20 +0530

    Add ClientCredentialsOAuth class for OAuth token fetch
    Introduces ClientCreadentialsOAuth.java to handle OAuth client credentials flow. Includes setup for RestAssured, a test to fetch the access token, and a placeholder for course details retrieval.

---
+++ DemoProject/src/ClientCreadentialsOAuth.java
@@ -0,0 +1,41 @@
+import static io.restassured.RestAssured.given;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import files.reusableMethods;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+public class ClientCreadentialsOAuth {
+
+	String accessToken;
+	
+	@BeforeTest
+	public void setup() throws IOException {
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+	}
+
+	@Test(priority = 1)
+	public void FetchingToken() {
+		String TokenResponse = given().log().all().formParam("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
+		.formParam("client_secret", "erZOWM9g3UtwNRj340YYaK_W")
+		.formParam("grant_type", "client_credentials")
+		.formParam("scope", "trust")
+		.when().post("/oauthapi/oauth2/resourceOwner/token")
+		.then().log().all().assertThat().statusCode(200).extract().response().asString();
+		
+		JsonPath jp1 = reusableMethods.rawToJson(TokenResponse);
+		accessToken = jp1.getString("access_token");
+		System.out.println(accessToken);
+	}
+	
+	@Test(priority = 2)
+	public void GetCourseDetails() {
+		
+	}
+	
+}
