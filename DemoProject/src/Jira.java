66fdfa604b64f1dad29c42adb53580e572f18f64
Akshay Harugop <99660258+AkshayHarugop@users.noreply.github.com>
2025-10-19 13:10:37 +0530

    Refactor Jira tests for attachment handling
    Replaces bug deletion with attachment removal, adds support for file upload to Jira issues, and updates variable names for clarity. The deleteBug method is commented out and a new removeAddedAttachmentToBug method is introduced to delete attachments using their ID.

--- DemoProject/src/Jira.java
+++ DemoProject/src/Jira.java
@@ -1,5 +1,8 @@
 import static io.restassured.RestAssured.given;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -9,39 +12,57 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
 public class Jira {
-	
-	String ID;
-	
+
+	String createBugID;
+	String attachmentId;
+
 	@BeforeTest
-	public void setup() {
-		RestAssured.baseURI= "https://akshayharugop.atlassian.net";
+	public void setup() throws IOException {
+		RestAssured.baseURI = "https://akshayharugop.atlassian.net";
+
 	}
-	
+
 	@Test(priority = 1)
 	public void createBug() {
-		String createBugResponse = given().log().all().header("Accept","application/json").header("Content-Type","application/json").header("Authorization","Basic YWtzaGF5aGFydWdvcEBnbWFpbC5jb206QVRBVFQzeEZmR0YwS093V3Jlc3g4bmRaSGxGYnNqQkZpczhEUUJDZVlKbk1aR1E0eXJlYXZ2VEZJUHUxRC1jOHpZLUFBek84dm5iNG5XWXhqdTF3dUlmRkxIdU9oU1M4WUJCWTlQWWxTMHk5ODR4NlFzTzhIN0kzZnp5WnNVWlF6VkhDeDlURHoyNUFaQllSOFBVel8yZ2owcFpZS083TWNiRGNCbHg5cktsZzF2enlPakN1dXdzPTJCODUyQUFD")
-		.body(payload.jiraCreateDefect())
-		.when().post("/rest/api/2/issue")
-		.then().log().all().assertThat().statusCode(201).extract().response().asString();
+		String createBugResponse = given().log().all().header("Accept", "application/json")
+				.header("Content-Type", "application/json")
+				.header("Authorization",
+						"Basic YWtzaGF5aGFydWdvcEBnbWFpbC5jb206QVRBVFQzeEZmR0YwS093V3Jlc3g4bmRaSGxGYnNqQkZpczhEUUJDZVlKbk1aR1E0eXJlYXZ2VEZJUHUxRC1jOHpZLUFBek84dm5iNG5XWXhqdTF3dUlmRkxIdU9oU1M4WUJCWTlQWWxTMHk5ODR4NlFzTzhIN0kzZnp5WnNVWlF6VkhDeDlURHoyNUFaQllSOFBVel8yZ2owcFpZS083TWNiRGNCbHg5cktsZzF2enlPakN1dXdzPTJCODUyQUFD")
+				.body(payload.jiraCreateDefect()).when().post("/rest/api/2/issue").then().log().all().assertThat()
+				.statusCode(201).extract().response().asString();
 		JsonPath jp1 = reusableMethods.rawToJson(createBugResponse);
-		ID = jp1.getString("id");
+		createBugID = jp1.getString("id");
 	}
-	
+
 	@Test(priority = 2)
-	public void addAttachementToBug() {
-		given().log().all().header("Authorization","Basic YWtzaGF5aGFydWdvcEBnbWFpbC5jb206QVRBVFQzeEZmR0YwS093V3Jlc3g4bmRaSGxGYnNqQkZpczhEUUJDZVlKbk1aR1E0eXJlYXZ2VEZJUHUxRC1jOHpZLUFBek84dm5iNG5XWXhqdTF3dUlmRkxIdU9oU1M4WUJCWTlQWWxTMHk5ODR4NlFzTzhIN0kzZnp5WnNVWlF6VkhDeDlURHoyNUFaQllSOFBVel8yZ2owcFpZS083TWNiRGNCbHg5cktsZzF2enlPakN1dXdzPTJCODUyQUFD")
-		.when().delete("/rest/api/2/issue/"+ID+"/attachments")
-		.then().log().all().assertThat().statusCode(200);
+	public void addAttachementToBug() throws IOException {
+		String addAttachementResponse = given().log().all().header("X-Atlassian-Token", "no-check").header(
+				"Authorization",
+				"Basic YWtzaGF5aGFydWdvcEBnbWFpbC5jb206QVRBVFQzeEZmR0YwS093V3Jlc3g4bmRaSGxGYnNqQkZpczhEUUJDZVlKbk1aR1E0eXJlYXZ2VEZJUHUxRC1jOHpZLUFBek84dm5iNG5XWXhqdTF3dUlmRkxIdU9oU1M4WUJCWTlQWWxTMHk5ODR4NlFzTzhIN0kzZnp5WnNVWlF6VkhDeDlURHoyNUFaQllSOFBVel8yZ2owcFpZS083TWNiRGNCbHg5cktsZzF2enlPakN1dXdzPTJCODUyQUFD")
+				.multiPart("file", new File("C:\\Users\\aha5\\Downloads\\Gemini_Generated_Image_furm4pfurm4pfurm.png"))
+				.log().all().when().post("/rest/api/2/issue/" + createBugID + "/attachments").then().log().all()
+				.assertThat().statusCode(200).extract().response().asString();
+		JsonPath jp2 = reusableMethods.rawToJson(addAttachementResponse);
+		attachmentId = jp2.getString("id");
 	}
-	
+
 	@Test(priority = 3)
-	public void deleteBug() {
-		System.out.println(ID);
-		given().log().all().header("Accept","application/json").header("Content-Type","application/json").header("Authorization","Basic YWtzaGF5aGFydWdvcEBnbWFpbC5jb206QVRBVFQzeEZmR0YwS093V3Jlc3g4bmRaSGxGYnNqQkZpczhEUUJDZVlKbk1aR1E0eXJlYXZ2VEZJUHUxRC1jOHpZLUFBek84dm5iNG5XWXhqdTF3dUlmRkxIdU9oU1M4WUJCWTlQWWxTMHk5ODR4NlFzTzhIN0kzZnp5WnNVWlF6VkhDeDlURHoyNUFaQllSOFBVel8yZ2owcFpZS083TWNiRGNCbHg5cktsZzF2enlPakN1dXdzPTJCODUyQUFD")
-		.when().delete("rest/api/2/issue/"+ID+"")
-		.then().log().all().assertThat().statusCode(204);
-		
+	public void removeAddedAttachmentToBug() {
+		System.out.println(attachmentId);
+		given().log().all().header("Accept", "application/json").header("Content-Type", "application/json").header(
+				"Authorization",
+				"Basic YWtzaGF5aGFydWdvcEBnbWFpbC5jb206QVRBVFQzeEZmR0YwS093V3Jlc3g4bmRaSGxGYnNqQkZpczhEUUJDZVlKbk1aR1E0eXJlYXZ2VEZJUHUxRC1jOHpZLUFBek84dm5iNG5XWXhqdTF3dUlmRkxIdU9oU1M4WUJCWTlQWWxTMHk5ODR4NlFzTzhIN0kzZnp5WnNVWlF6VkhDeDlURHoyNUFaQllSOFBVel8yZ2owcFpZS083TWNiRGNCbHg5cktsZzF2enlPakN1dXdzPTJCODUyQUFD")
+				.when().delete("/rest/api/3/attachment/" + attachmentId).then().log().all().assertThat()
+				.statusCode(204);
 	}
-	
-	
+
+//	@Test(priority = 4)
+//	public void deleteBug() {
+//		given().log().all().header("Accept", "application/json").header("Content-Type", "application/json").header(
+//				"Authorization",
+//				"Basic YWtzaGF5aGFydWdvcEBnbWFpbC5jb206QVRBVFQzeEZmR0YwS093V3Jlc3g4bmRaSGxGYnNqQkZpczhEUUJDZVlKbk1aR1E0eXJlYXZ2VEZJUHUxRC1jOHpZLUFBek84dm5iNG5XWXhqdTF3dUlmRkxIdU9oU1M4WUJCWTlQWWxTMHk5ODR4NlFzTzhIN0kzZnp5WnNVWlF6VkhDeDlURHoyNUFaQllSOFBVel8yZ2owcFpZS083TWNiRGNCbHg5cktsZzF2enlPakN1dXdzPTJCODUyQUFD")
+//				.when().delete("rest/api/2/issue/" + createBugID + "").then().log().all().assertThat().statusCode(204);
+//
+//	}
+
 }
